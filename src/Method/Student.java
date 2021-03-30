7024686dd75944cb18fdf23d5f841696f39d44b3
현중 강 <ghyung102@cu.ac.kr>
2021-03-30 17:12:43 +0900

    함수 구현하고 호출하기 소스 추가

---
+++ src/Method/Student.java
@@ -0,0 +1,13 @@
+package Method;
+
+public class Student {
+	int studentID;
+	String studentName;
+	int grade;
+	String address;
+	
+	public void showStduentInfo() {
+		System.out.println(studentName + "," + address);
+	}
+
+}
